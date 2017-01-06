048a41468801b51bc92fe974d51d8ac70440fce8
lzambella <lukezambella@gmail.com>
2017-01-06 17:19:26 -0500

    Update code

--- Discord-NetCore/src/Discord-NetCore/Modules/TextModule.cs
+++ Discord-NetCore/src/Discord-NetCore/Modules/TextModule.cs
@@ -10,32 +10,32 @@ namespace Discord_NetCore.Modules
     public class TextModule : ModuleBase
     {
         [Command("poke"), Summary("Annoy someone")]
-        public async Task Annoy(IUserMessage msg, [Summary("Mention")]string nickMention = null)
+        public async Task Annoy([Summary("Mention")]string nickMention = null)
         {
             if (nickMention == null)
             {
-                await msg.Channel.SendMessageAsync("You didn't mention anyone!");
+                await ReplyAsync("You didn't mention anyone!");
             }
             else
             {
                 var id = ulong.Parse(Program.Database.ParseString(nickMention));
-                var guild = Program.Client.GetGuild(msg.Channel.Id);
+                var guild = Program.Client.GetGuild(Context.Channel.Id);
                 var userDM = await guild.GetUser(id).CreateDMChannelAsync();
                 for (var x = 0; x < 5; x++)
                     await userDM.SendMessageAsync("You are being annoying!!!! Get On!!!!", true);
             }
         }
         [Command("8ball"), Summary("Ask me for advice.")]
-        public async Task Ball(IUserMessage msg, [Summary("Question")] string s = null)
+        public async Task Ball([Summary("Question")] string s = null)
         {
             if (s == null)
-                await msg.Channel.SendMessageAsync("I didn't get a question.");
+                await ReplyAsync("I didn't get a question.");
             else
             {
                 var arr = new string[] { "Yes", "No", "Maybe", "Ask again", "I'm not answering that", "That question sucks" };
                 var rand = new Random();
                 var index = rand.Next(arr.Length);
-                await msg.Channel.SendMessageAsync(arr[index]);
+                await ReplyAsync(arr[index]);
             }
         }
         [Command("purge"), Summary("Delete a number of messages")]
