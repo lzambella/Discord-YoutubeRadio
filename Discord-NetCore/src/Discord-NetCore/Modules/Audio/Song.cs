d3acc0adbb68ec6fbd53611a76d626bf4e1de151
lzambella <lukezambella@gmail.com>
2017-05-07 16:30:32 -0400

    .

--- Discord-NetCore/src/Discord-NetCore/Modules/Audio/Song.cs
+++ Discord-NetCore/src/Discord-NetCore/Modules/Audio/Song.cs
@@ -66,7 +66,7 @@ namespace Discord_NetCore.Modules.Audio
             {
                 process = Process.Start(new ProcessStartInfo
                 {
-                    FileName = "youtube-dl",
+                    FileName = "./youtube-dl",
                     Arguments = $"-e --get-duration {Url} ",
                     UseShellExecute = false,
                     RedirectStandardOutput = true,
