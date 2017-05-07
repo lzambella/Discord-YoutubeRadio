1e6e9fc809a6266973346a9379e5c9697ca0d5db
lzambella <lukezambella@gmail.com>
2017-05-07 17:18:10 -0400

    .

--- Discord-NetCore/src/Discord-NetCore/Modules/Audio/MusicPlayer.cs
+++ Discord-NetCore/src/Discord-NetCore/Modules/Audio/MusicPlayer.cs
@@ -370,7 +370,7 @@ namespace Discord_NetCore.Modules.Audio
                         _process = Process.Start(new ProcessStartInfo
                         {
                             FileName = "bash",
-                            Arguments = $"youtube-dl -q -o - {url} | ffmpeg -i - -f s16le -ar 48000 -ac 2 -loglevel quiet pipe:1 ",
+                            Arguments = $"/app/heroku_output/youtube-dl -q -o - {url} | /app/heroku_output/ffmpeg -i - -f s16le -ar 48000 -ac 2 -loglevel quiet pipe:1 ",
                             UseShellExecute = false,
                             RedirectStandardOutput = true,
                             RedirectStandardError = false,
